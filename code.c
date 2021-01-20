409b031c14adf2be4e7c5b15b54a859d70070aed
Nivesh Rohankar <71220063+FR4NCOT@users.noreply.github.com>
2021-01-20 16:14:07 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+        char c;
+        printf("Enter the Character value:\n");
+        scanf("%c", &c);
+        
+        printf("ASCII value of %c=%d", c,c);
+    return 0;
+}
+
+/*int main()
+{
+        int c;
+        printf("Enter the ASCII:\n");
+        scanf("%d", &c);
+        
+        printf("The Character value of %d=%c", c,c);
+    return 0;
+}*/
